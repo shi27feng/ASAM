2d67c3a77dc1cf998f6252f957f113ff8ae8e131
Feng Shi <shi.feng@cs.ucla.edu>
2021-11-04 09:08:59 +0800

    Update example_cifar.py

--- example_cifar.py
+++ example_cifar.py
@@ -8,6 +8,7 @@ from timm.loss import LabelSmoothingCrossEntropy
 from homura.vision.models.cifar_resnet import wrn28_2, wrn28_10, resnet20, resnet56, resnext29_32x4d
 from asam import ASAM, SAM
 
+
 def load_cifar(data_loader, batch_size=256, num_workers=2):
     if data_loader == CIFAR10:
         mean = (0.4914, 0.4822, 0.4465)
@@ -18,26 +19,26 @@ def load_cifar(data_loader, batch_size=256, num_workers=2):
 
     # Transforms
     train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
-                         transforms.RandomHorizontalFlip(),
-                         transforms.ToTensor(),
-                         transforms.Normalize(mean, std)])
+                                          transforms.RandomHorizontalFlip(),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean, std)])
 
     test_transform = transforms.Compose([
-                         transforms.ToTensor(),
-                         transforms.Normalize(mean, std)
-                         ])
- 
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+
     # DataLoader
     train_set = data_loader(root='./data', train=True, download=True, transform=train_transform)
     test_set = data_loader(root='./data', train=False, download=True, transform=test_transform)
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, 
-            num_workers=num_workers)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, 
-            num_workers=num_workers)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True,
+                              num_workers=num_workers)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False,
+                             num_workers=num_workers)
     return train_loader, test_loader
-   
 
-def train(args): 
+
+def train(args):
     # Data Loader
     train_loader, test_loader = load_cifar(eval(args.dataset), args.batch_size)
     num_classes = 10 if args.dataset == 'CIFAR10' else 100
@@ -46,10 +47,10 @@ def train(args):
     model = eval(args.model)(num_classes=num_classes).cuda()
 
     # Minimizer
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, 
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr,
                                 momentum=args.momentum, weight_decay=args.weight_decay)
     minimizer = eval(args.minimizer)(optimizer, model, rho=args.rho, eta=args.eta)
-    
+
     # Learning Rate Scheduler
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(minimizer.optimizer, args.epochs)
 
@@ -105,7 +106,7 @@ def train(args):
             loss /= cnt
             accuracy *= 100. / cnt
         if best_accuracy < accuracy:
-           best_accuracy = accuracy
+            best_accuracy = accuracy
         print(f"Epoch: {epoch}, Test accuracy:  {accuracy:6.2f} %, Test loss:  {loss:8.5f}")
     print(f"Best test accuracy: {best_accuracy}")
 
@@ -125,7 +126,7 @@ if __name__ == "__main__":
     parser.add_argument("--eta", default=0.0, type=float, help="Eta for ASAM.")
     args = parser.parse_args()
     assert args.dataset in ['CIFAR10', 'CIFAR100'], \
-            f"Invalid data type. Please select CIFAR10 or CIFAR100"
+        f"Invalid data type. Please select CIFAR10 or CIFAR100"
     assert args.minimizer in ['ASAM', 'SAM'], \
-            f"Invalid minimizer type. Please select ASAM or SAM"
+        f"Invalid minimizer type. Please select ASAM or SAM"
     train(args)
